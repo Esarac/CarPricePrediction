22a9a1ffd7b19b9de023dbc8385d6d15d3ded6d1
Esteban Ariza Acosta <48232743+Esarac@users.noreply.github.com>
2021-09-15 11:42:34 -0500

    MultiThreading :heavy_multiplication_x:

--- webScraping.py
+++ webScraping.py
@@ -1,62 +1,72 @@
+from multiprocessing import Pool
+from time import sleep
 from helium import *
 from bs4 import BeautifulSoup
 import pandas as pd
+import multiprocessing as mp
 
-browser = start_firefox(headless = True)
+#--------------------Constants--------------------
+THREAD_QUANTITY = int(mp.cpu_count()/2) #Change to apply a different number of threads
+PAGE_QUANTITY = 688 #688 is the actual number of pages in this website
 
-#columnsname = ['NOMBRE', 'SUBTITULO', 'PRECIO', 'PRECIO MENSUAL', 'KILOMETRAJE', 'ANIO', 'ESTADO', 'UBICACIÓN', 'PLACA', 'TIPO DE CAJA', 'CILINDRAJE', 'COMBUSTIBLE',  'COLOR', 'AIRBAGS', 'DIRECCIÓN', 'CARROCERÍA', 'PUERTAS']
-columnsname = ['NOMBRE', 'SUBTITULO', 'PRECIO', 'PRECIO MENSUAL', 'KILOMETRAJE', 'ANIO', 'TIPO DE CAJA', 'CILINDRAJE', 'COMBUSTIBLE', 'COLOR', 'ESTADO', 'UBICACIÓN', 'DIRECCIÓN', 'PLACA', 'PUERTAS', 'AIRBAGS', 'IMAGEN']
-allCars = []
+#--------------------Methods--------------------
+def get_data(columnsname, threadNumber):
 
-for x in range(1, 2):
-    url = 'https://www.carroya.com/resultados/automoviles-y-camionetas?page=' + str(x)
-    go_to(url)
-    soup = BeautifulSoup(browser.page_source, 'html.parser')
-    cars = soup.find_all('div', class_='contentCurrentCard')
+    errors = 0
+    browser = start_firefox(headless = True)
+    allCars = []
 
-    for y in range(3,len(cars)):
-        try:
-            car = cars[y]
-            carTemp = {}
+    for x in range(threadNumber, (PAGE_QUANTITY + 1), THREAD_QUANTITY):
+        url = 'https://www.carroya.com/resultados/automoviles-y-camionetas?page=' + str(x)
+        go_to(url)
+        soup = BeautifulSoup(browser.page_source, 'html.parser')
+        sleep(3)
+        cars = soup.find_all('div', class_='contentCurrentCard')
 
-            for g in columnsname:
-                carTemp[g] = ''
+        for y in range(3,len(cars)):
+            try:
+                car = cars[y]
+                carTemp = {}
 
-            #Data
-            carTemp['NOMBRE'] = car.find('h3', class_='h3P titleCard').text
-            carTemp['SUBTITULO'] = car.find('h4', class_='subtitleCard').text
-            carTemp['PRECIO'] = car.find('h2', class_='h2P priceCard').text
-            carTemp['PRECIO MENSUAL'] = car.find('h3', class_='mounthlyPriceCard').text
-            carTemp['IMAGEN'] = car.find('img', class_='imageCard')['src']
+                for g in columnsname:
+                    carTemp[g] = ''
 
-            #Car Detail Page
-            detailUrl = 'https://www.carroya.com' + car.find('a', href = True)['href']
-            go_to(detailUrl)
-            detail = BeautifulSoup(browser.page_source, 'html.parser')
-            features = detail.find('div', class_='features')
+                #Data
+                carTemp['NOMBRE'] = car.find('h3', class_='h3P titleCard').text
+                carTemp['SUBTITULO'] = car.find('h4', class_='subtitleCard').text
+                carTemp['PRECIO'] = car.find('h2', class_='h2P priceCard').text
+                carTemp['PRECIO MENSUAL'] = car.find('h3', class_='mounthlyPriceCard').text
+                carTemp['IMAGEN'] = car.find('img', class_='imageCard')['src']
 
-            names = features.find_all('h5', class_='name')
-            descriptions = features.find_all('h4', class_='description')
+                #Car Detail Page
+                detailUrl = 'https://www.carroya.com' + car.find('a', href = True)['href']
+                go_to(detailUrl)
+                detail = BeautifulSoup(browser.page_source, 'html.parser')
+                sleep(1)
+                features = detail.find('div', class_='features')
 
-            #Data
-            carTemp['KILOMETRAJE'] = detail.find('h3', class_='h3P kilometers').text
-            carTemp['ANIO'] = detail.find('h3', class_='h3P year').text
+                names = features.find_all('h5', class_='name')
+                descriptions = features.find_all('h4', class_='description')
 
-            for c in range(len(names)):
-                if names[c].text in set(columnsname):
-                    carTemp[names[c].text] = str(descriptions[c].text)
+                #Data
+                carTemp['KILOMETRAJE'] = detail.find('h3', class_='h3P kilometers').text
+                carTemp['ANIO'] = detail.find('h3', class_='h3P year').text
 
-            allCars.append(carTemp)
-            print("PAG "+str(x)+", CAR "+str(y-2))
-        except AttributeError:
-            print("INVALID - PAG "+str(x)+", CAR "+str(1+y))
+                for c in range(len(names)):
+                    if names[c].text in set(columnsname):
+                        carTemp[names[c].text] = str(descriptions[c].text)
 
-for c in allCars:
-    print(c.keys())
+                allCars.append(carTemp)
+                print("PAG "+str(x)+", CAR "+str(y-2))#Print actual car
+            except AttributeError:
+                print("INVALID - PAG "+str(x)+", CAR "+str(1+y))#Print actual cars (invalid)
+                errors = errors + 1
 
-pd.DataFrame.from_dict(data=allCars, orient='columns').to_csv('data.csv', header=True)
+    print("Thread "+str(threadNumber)+" errors: "+ str(errors))#Print errors in thread
 
-kill_browser()
+    kill_browser()
+
+    return allCars
 
 def download_img(x, y, z):
     print(x + '-' + y + '-' + str(z))
@@ -65,3 +75,20 @@ def load_img():
     df = pd.read_csv('data.csv')
     for x, y, z in zip(df['NOMBRE'], df['IMAGEN'], df['Unnamed: 0']):
         download_img(x, y, z)
+
+if __name__ == "__main__":
+    #--------------------Logic--------------------
+    columnsname = ['NOMBRE', 'SUBTITULO', 'PRECIO', 'PRECIO MENSUAL', 'KILOMETRAJE', 'ANIO', 'TIPO DE CAJA', 'CILINDRAJE', 'COMBUSTIBLE', 'COLOR', 'ESTADO', 'UBICACIÓN', 'DIRECCIÓN', 'PLACA', 'PUERTAS', 'AIRBAGS', 'IMAGEN']
+
+    pool = Pool(THREAD_QUANTITY)
+    
+    tuples = []
+    for t in range(1, THREAD_QUANTITY + 1):
+        tuples.append((columnsname, t))
+
+    carsMatrix = pool.starmap(get_data, tuples)
+    allCars = []
+    for c in carsMatrix:
+        allCars += c
+    
+    pd.DataFrame.from_dict(data=allCars, orient='columns').to_csv('data.csv', header=True)
