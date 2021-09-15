53f58633661a879d797a1fcad6a0897e4120719c
VoodLyc <j.sebasgr032@gmail.com>
2021-09-15 02:39:24 -0500

    the csv saves the link to the image

--- webScraping.py
+++ webScraping.py
@@ -5,10 +5,10 @@ import pandas as pd
 browser = start_firefox(headless = True)
 
 #columnsname = ['NOMBRE', 'SUBTITULO', 'PRECIO', 'PRECIO MENSUAL', 'KILOMETRAJE', 'ANIO', 'ESTADO', 'UBICACIÓN', 'PLACA', 'TIPO DE CAJA', 'CILINDRAJE', 'COMBUSTIBLE',  'COLOR', 'AIRBAGS', 'DIRECCIÓN', 'CARROCERÍA', 'PUERTAS']
-columnsname = ['NOMBRE', 'SUBTITULO', 'PRECIO', 'PRECIO MENSUAL', 'KILOMETRAJE', 'ANIO', 'TIPO DE CAJA', 'CILINDRAJE', 'COMBUSTIBLE', 'COLOR', 'ESTADO', 'UBICACIÓN', 'DIRECCIÓN', 'PLACA', 'PUERTAS', 'AIRBAGS']
+columnsname = ['NOMBRE', 'SUBTITULO', 'PRECIO', 'PRECIO MENSUAL', 'KILOMETRAJE', 'ANIO', 'TIPO DE CAJA', 'CILINDRAJE', 'COMBUSTIBLE', 'COLOR', 'ESTADO', 'UBICACIÓN', 'DIRECCIÓN', 'PLACA', 'PUERTAS', 'AIRBAGS', 'IMAGEN']
 allCars = []
 
-for x in range(1, 10):
+for x in range(1, 2):
     url = 'https://www.carroya.com/resultados/automoviles-y-camionetas?page=' + str(x)
     go_to(url)
     soup = BeautifulSoup(browser.page_source, 'html.parser')
@@ -27,6 +27,7 @@ for x in range(1, 10):
             carTemp['SUBTITULO'] = car.find('h4', class_='subtitleCard').text
             carTemp['PRECIO'] = car.find('h2', class_='h2P priceCard').text
             carTemp['PRECIO MENSUAL'] = car.find('h3', class_='mounthlyPriceCard').text
+            carTemp['IMAGEN'] = car.find('img', class_='imageCard')['src']
 
             #Car Detail Page
             detailUrl = 'https://www.carroya.com' + car.find('a', href = True)['href']
@@ -40,7 +41,7 @@ for x in range(1, 10):
             #Data
             carTemp['KILOMETRAJE'] = detail.find('h3', class_='h3P kilometers').text
             carTemp['ANIO'] = detail.find('h3', class_='h3P year').text
-            
+
             for c in range(len(names)):
                 if names[c].text in set(columnsname):
                     carTemp[names[c].text] = str(descriptions[c].text)
@@ -56,3 +57,11 @@ for c in allCars:
 pd.DataFrame.from_dict(data=allCars, orient='columns').to_csv('data.csv', header=True)
 
 kill_browser()
+
+def download_img(x, y, z):
+    print(x + '-' + y + '-' + str(z))
+
+def load_img():
+    df = pd.read_csv('data.csv')
+    for x, y, z in zip(df['NOMBRE'], df['IMAGEN'], df['Unnamed: 0']):
+        download_img(x, y, z)
