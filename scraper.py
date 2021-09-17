a4da937f15724049a0264542ffce986e5568dd97
mavaldot <48870467+mavaldot@users.noreply.github.com>
2021-09-17 12:11:57 -0500

    Fix bug in scraper.py
    Now the csv rows are formatted with the correct number of columns even when values are missing in the website

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 import csv
+import unidecode
 
 from bs4 import BeautifulSoup
 from multiprocessing import Pool
@@ -29,16 +30,21 @@ def car_scraper(year_start, year_end):
                 car_page = requests.get(link['href'])
                 car_soup = BeautifulSoup(car_page.content, 'html.parser')
 
-                car_data = []
+                car_data = {}
 
                 for title in car_soup.select('h1[class="ui-pdp-title"]'):
-                    car_data.append(title.text)
+                    car_data['Titulo'] = title.text
 
-                for value in car_soup.select('span[class="andes-table__column--value"]'):
-                    car_data.append(value.text)
+                for value in car_soup.select('tr[class="andes-table__row"]'):
+                    key = unidecode.unidecode(value.th.text)
+                    car_data[key] = value.td.text
+
+                for img in car_soup.select('img[class="ui-pdp-image ui-pdp-gallery__figure__image"]'):
+                    car_data['Img'] = img['src']
 
                 for price in car_soup.select('span[class="price-tag-fraction"]'):
-                    car_data.append(price.text)
+                    car_data['Precio'] = price.text
+
 
                 data.append(car_data)
     
@@ -47,12 +53,30 @@ def car_scraper(year_start, year_end):
 def create_csv():
     pool = Pool(4)
 
-    result = pool.starmap(car_scraper, [(1938, 1958), (1958, 1978), (1978, 1998), (1998, 2008), (2008, 2014), (2014, 2018), (2018, 2022)])
+    result = pool.starmap(car_scraper, [(1938, 1958), (1958, 1978), (1978, 1998), (1998, 2008), (2008, 2010), (2010, 2014), (2014, 2018), (2018, 2020), (2020, 2022)])
 
     with open('data.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         
+        columns = ['Titulo', 'Marca', 'Modelo', 'Ano', 'Color', 
+        'Tipo de combustible', 'Puertas', 'Transmision', 'Motor', 'Tipo de carroceria', 'Kilometros', 'Img', 'Precio']
+
+        writer.writerow(columns)
+
         for item in result:
-            writer.writerows(item)
-        
-create_csv()
+            for car in item:
+
+                row = []
+
+                for val in columns:  
+
+                    if val in car.keys():
+                        row.append(car[val])
+                    else:
+                        row.append('N/A')
+
+                writer.writerow(row)
+
+                
+if __name__ == '__main__':
+    create_csv()
